c540117a6f1cb5004f866ab92334ab7bfa2690a8
kadir-zeren <k4dzeren@gmail.com>
2023-07-23 13:32:48 +0300

    V3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.scss";
+import Header from "./components/header/Header";
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <Header />
     </div>
   );
 }
